279c77d69182372d1832e5d35450b98ba3e30f7b
alvaroarchilla <alvaroarchilla@gmail.com>
2023-10-14 20:03:08 +0200

    First functional prototype

--- PlantOnator.ino
+++ PlantOnator.ino
@@ -8,21 +8,48 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
+#define SOIL_MOISTURE_MIN 30
 
+//Pin definition
+int irrigationPin = 12;
+int touchSensorPin = 13;
+const int sensor_pin = A0;  /* Connect Soil moisture analog sensor pin to A0 of NodeMCU */
+
+//AHT variables
+float temperature;
+float humidity;
+
+//Soil humidity sensor variables
+float moisture_percentage;
+bool irrigationCurrentState;
+
+//Screen variables
+bool soilMoistureViewActive = false;
+unsigned long oledScreenViewTime = 2500;
+unsigned long checkDataPrevMillis;
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 Adafruit_AHT10 aht;
 
-float temperature;
-float humidity;
-float moisture_percentage;
-unsigned long oledScreenViewTime = 2000;
-unsigned long checkDataPrevMillis;
-bool soilMoistureViewActive = false;
-int irrigationPin = 12;
-bool irrigationCurrentState;
+//Irrigation timer variables
+unsigned long checkIrrigationPrevMillis;
+unsigned long lastcheckIrrigationMillis;
+unsigned long checkIrrigationTimer = 900000; //timer until change irrigation state to on 40 min
+unsigned long irrigationTimerON = 60000; //timer until change irrigation state to on 40 min
+bool checkIrrigationFlag = true;
 
-const int sensor_pin = A0;  /* Connect Soil moisture analog sensor pin to A0 of NodeMCU */
+// Arduino function that converts milliseconds into HH:MM:SS format
+String msToHMS(unsigned long milliseconds) {
+  int seconds = milliseconds / 1000;
+  int minutes = seconds / 60;
+  int hours = minutes / 60;
+
+  String hms = String(hours) + ":";
+  hms += String(minutes % 60) + ":";
+  hms += String(seconds % 60);
+
+  return hms;
+}
 
 void initOled() {
   delay(1000);  // wait a second
@@ -48,7 +75,7 @@ void sendNotificationOled(String message)
 
 }
 
-void updateOled(float temperature, float humidity, float moisture_percentage) {
+void updateOled(float temperature, float humidity, float moisture_percentage, bool irrigationCurrentState) {
   if (soilMoistureViewActive == false) {
     display.clearDisplay();
     display.setTextSize(1);
@@ -75,11 +102,28 @@ void updateOled(float temperature, float humidity, float moisture_percentage) {
     display.clearDisplay();
     display.setTextSize(1);
     display.setCursor(0, 0);
-    display.print("Soil Moisture (Percen.)");
+    display.print("SoilMoist - min= ");
+       display.print(SOIL_MOISTURE_MIN);
     display.setTextSize(2);
     display.setCursor(0, 10);
     display.print(moisture_percentage);
     display.print(" %");
+    display.setTextSize(1);
+    display.setCursor(0, 35);
+    display.print("CurrentStatus: ");
+    display.print(irrigationCurrentState);
+    display.setTextSize(1);
+    display.setCursor(0, 50);
+    //If it is irrigating (irrigationCurrentState==0) shows the remaining time to stop irrigation
+    if (!irrigationCurrentState) {
+      display.print("Irrigating: ");
+      display.print(msToHMS(lastcheckIrrigationMillis + irrigationTimerON - millis()));
+    }
+    //If it not irrigating (irrigationCurrentState==1) shows the remaining time to check moisture_percentage level
+    else {
+      display.print("TimeToCheck: ");
+      display.print(msToHMS(checkIrrigationPrevMillis + checkIrrigationTimer - millis()));
+    }
     display.display();
   }
 }
@@ -99,45 +143,80 @@ float getMoisturePercentage() {
   float moisture_percentage;
   moisture_percentage = ( 100.00 - ( (analogRead(sensor_pin) / 1023.00) * 100.00 ) );
 
-  Serial.print("Soil Moisture(in Percentage) = ");
-  Serial.print(moisture_percentage);
-  Serial.println("%");
-
+  //Serial.print("Soil Moisture(in Percentage) = ");
+  //Serial.print(moisture_percentage);
+  //Serial.println("%");
   delay(100);
   return moisture_percentage;
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+void changeIrrigationState(bool irrigationCurrentState)
+{
+  bool irrigationState;
+  if (irrigationCurrentState) {
+    irrigationState = 0;
+  }
+  else {
+    irrigationState = 1;
+  }
+  digitalWrite(irrigationPin, irrigationState);
+  Serial.println("IrrigationStatePin changed to: ");
+  Serial.println(irrigationState);
+  delay(500);
+}
 
+void setup() {
+  Serial.begin(115200);
   // Initialize sensors
   initOled();
   initAHT();
   pinMode(irrigationPin, OUTPUT);
+  pinMode(touchSensorPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   sensors_event_t humidity, temp;
   aht.getEvent(&humidity, &temp); // populate temp and humidity objects with fresh data
   moisture_percentage = getMoisturePercentage();
-  if (moisture_percentage > 50) {
-  digitalWrite(irrigationPin, HIGH);
-  Serial.println("IrrigationStatePin changed to HIGH");
+  irrigationCurrentState = digitalRead(irrigationPin);
+  updateOled(temp.temperature, humidity.relative_humidity, moisture_percentage, irrigationCurrentState);
 
-  }
-  else {
-  digitalWrite(irrigationPin, LOW);
-  Serial.println("IrrigationStatePin changed to LOW");
+  if (digitalRead(touchSensorPin)) {
+    delay(200);
+    changeIrrigationState(digitalRead(irrigationPin));
   }
 
-  updateOled(temp.temperature, humidity.relative_humidity, moisture_percentage);
-
+  //Timer to check change between th einfor that is show in the screen
   if ((millis() - checkDataPrevMillis > oledScreenViewTime || checkDataPrevMillis == 0))
   {
-    checkDataPrevMillis = millis(); 
+    checkDataPrevMillis = millis();
     soilMoistureViewActive = !soilMoistureViewActive;
 
   }
+
+  //Timer to check irrigation status to avoid overwatering on slow response
+  if ((millis() - checkIrrigationPrevMillis > checkIrrigationTimer ))
+  {
+    Serial.println("Checking Irrigation");
+    checkIrrigationPrevMillis = millis();
+    checkIrrigationFlag = 1;
+  }
+
+  //Si no está regando pero el soilHumidity es menor que el esperado cambia el estado de irrigación a encendido
+  if ( irrigationCurrentState == 1 && moisture_percentage < SOIL_MOISTURE_MIN && checkIrrigationFlag)
+  {
+    Serial.println("Starting irrigation");
+    lastcheckIrrigationMillis = millis();
+    checkIrrigationFlag = 0;
+    changeIrrigationState(irrigationCurrentState);
+  }
+  //Si está regando comprueba el tiempo desde que empezó el riego y cuando sea mayor lo apaga
+  else if (irrigationCurrentState == 0 && (millis() - lastcheckIrrigationMillis >= irrigationTimerON) )
+  {
+    Serial.println("Turning off irrigation due timout");
+    lastcheckIrrigationMillis = millis();
+    checkIrrigationFlag = 0;
+    changeIrrigationState(irrigationCurrentState);
+  }
+
 }
